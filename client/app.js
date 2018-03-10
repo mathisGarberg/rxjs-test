8cb048345e7c160a13d05ab7b95ac0c7196fd7e2
Mathis Garberg <Mathis.Garberg@gmail.com>
2018-03-10 21:36:20 +0100

    Added cdn for bacon.js lib

--- client/app.js
+++ client/app.js
@@ -1,5 +1,9 @@
-const names = ['Mathis', 'Jonas'];
+// Bacon.js is a library for functional reactive prgramming. Or let's say it's a library for working with events
+// and dynamic values
+const clicks = $("h1").asEventStream("click");
 
-names.map((name) => {
-    console.log(name);
-});
+var plus = $("#plus").asEventStream("click").map(1)
+var minus = $("#minus").asEventStream("click").map(-1)
+var both = plus.merge(minus)
+
+both.onValue(function(val) { console.log(val) });
